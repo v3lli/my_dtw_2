bedc7508e6176e5d24179f63af34c38c934959df
v3lli <emekaveli@gmail.com>
2021-06-30 08:13:00 +0100

    added font awesome

--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {fas} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+library.add(fas)
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(store).use(router).component('fa', FontAwesomeIcon).mount('#app')
 
