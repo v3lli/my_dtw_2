b29de2f19135990c7d3addd3542b5e64926eff97
Velli <emekavelli@Vellis-MacBook-Pro.local>
2022-02-10 19:11:39 +0100

    API call to the albums collection done

---
+++ src/views/Albums.vue
@@ -0,0 +1,32 @@
+<template>
+<ul>
+</ul>
+</template>
+<script>
+import axios from 'axios'
+// @ is an alias to /src
+
+export default {
+  name: 'Albums',
+  data(){
+  return{
+    albums:null,
+    url:"http://127.0.0.1:3000/api/v1/albums"
+}},
+  methods:{
+    getalbums(){
+            axios.get(this.url)
+            .then((response)=>{
+                console.log(response.data.data)  
+                this.albums = response.data.data.albums
+                console.log(this.albums[0].name)
+            })
+        },
+  },
+  created(){
+    this.getalbums()
+  }
+}
+</script>
+<style scoped>
+</style>
